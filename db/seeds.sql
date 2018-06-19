4c272f9029e5363f99fcd644286aa407e31d8d6c
David <dph0718@hotmail.com>
2018-06-19 14:59:01 -0600

    db change

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,10 @@
 -- insert queries to populate burgers table with three entries
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("The Living Cow", false),
- ("The Moo", false),
- ("Tastes Like Feet", false),
- ("The Large Anndensharjje", false);
+INSERT INTO burgers (burger_name, devoured, image)
+VALUES ("The Ex-Cow Ibur", false, true),
+("The Play-Doh Burger", false, true),
+("The Vegan Bacon Cheeseburger", false, true),
+ ("The Large Anndensharjje", false, true),
+("The Playskool Burger Kit", false, true),
+("The Dog Chew Toy", false, true);
